da43540526b36557015762b6b7e37dea82977452
Ivan Ivanov <vankodj@students.softuni.bg>
2022-12-26 13:04:01 +0200

    Modifying

--- LapTopShop/src/main/java/exam/model/entities/WarrantyType.java
+++
@@ -1,5 +0,0 @@
-package exam.model.entities;
-
-public enum WarrantyType {
-    BASIC, PREMIUM, LIFETIME
-}
--- RealDealSkeleton/src/main/java/softuni/exam/models/entities/RatingType.java
+++
@@ -1,5 +0,0 @@
-package softuni.exam.models.entities;
-
-public enum RatingType {
-    GOOD, BAD, UNKNOWN
-}
